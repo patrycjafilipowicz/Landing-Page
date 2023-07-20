fd78675930887f456ef149da6516b4b345e9abcb
Pat <patrycja.filipowicz46@gmail.com>
2023-07-20 20:21:29 +0200

    changes

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="opening">
+        <div className="headerLogo">Header Logo</div>
+        <div className="introduction">
+          <h1>This app is about cats!</h1>
+          <p>Cats are the most beautiful animals in the world.</p>
+          <button>Sign up</button>
+          <div className="image">pictrue</div>
+          <div className="headers">
+            <div>link 1</div>
+            <div>link 2</div>
+            <div>link 3</div>
+          </div>
+        </div>
+      </div>
+      <div className="images"></div>
+      <div className="quote"></div>
+      <div className="closing"></div>
     </div>
   );
 }
