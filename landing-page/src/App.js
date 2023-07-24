1a2f8bb675c0ac36ef1c9f16bdf6a0ca02bae462
Pat <patrycja.filipowicz46@gmail.com>
2023-07-24 20:27:06 +0200

    changes

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,28 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <div className="opening">
-        <div className="headerLogo">Header Logo</div>
-        <div className="introduction">
-          <h1>This app is about cats!</h1>
-          <p>Cats are the most beautiful animals in the world.</p>
-          <button>Sign up</button>
-          <div className="image">pictrue</div>
-          <div className="headers">
-            <div>link 1</div>
-            <div>link 2</div>
-            <div>link 3</div>
-          </div>
-        </div>
-      </div>
-      <div className="images"></div>
-      <div className="quote"></div>
-      <div className="closing"></div>
-    </div>
-  );
+	return (
+		<div className="main-container">
+      <div className="black-background"></div>
+			<div className='container'>
+				<div className='opening'>
+					<div className='headerLogo'>
+						<div>Header logo</div>
+						<div className='headers'>
+							<div> link 1 </div>
+							<div> link 2 </div>
+							<div> link 3 </div>
+						</div>
+					</div>
+				</div>
+				<div className='introduction'>
+					<div></div>
+					<div>
+						<h1>This app is about cats!</h1>
+						<p>
+							Cats are the most beautiful animals in the world. Lorem Ipsum is
+							simply dummy text of the printing and typesetting industry. Lorem
+							Ipsum has been the industry's standard dummy text ever since the
+							1500s, when an unknown printer took a galley of type and scrambled
+							it to make a type specimen book.
+						</p>
+						<button>Sign up</button>
+					</div>
+					<div className='image'>pictrue</div>
+					<div></div>
+				</div>
+				<div className='images'></div>
+				<div className='quote'></div>
+				<div className='closing'></div>
+			</div>
+      <div className="black-background"></div>
+		</div>
+	);
 }
 
 export default App;
