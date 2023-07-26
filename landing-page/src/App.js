d76ff57571094baa2a6cad2fe81fb09e909a7fe7
Pat <patrycja.filipowicz46@gmail.com>
2023-07-26 19:24:34 +0200

    changes

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -3,35 +3,53 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="main-container">
-      <div className="black-background"></div>
+		<div className='main-container'>
+			<div className='black-background'></div>
 			<div className='container'>
-					< div className='headerLogo'>
-						<div>Header logo</div>
-						<div className='headers'>
-							<div> link 1 </div>
-							<div> link 2 </div>
-							<div> link 3 </div>
-						</div>
+				<div className='headerLogo'>
+					<div>Header logo</div>
+					<div className='headers'>
+						<div> link 1 </div>
+						<div> link 2 </div>
+						<div> link 3 </div>
+					</div>
 				</div>
 				<div className='introduction'>
 					<div></div>
 					<div>
-						<h1>This app is about cats!</h1>
-						<p>
-							Cats are the most beautiful animals in the world. Lorem Ipsum is
-							simply dummy text of the printing and typesetting industry.
-						</p>
+						<h1 className='about'>This app is about cats!</h1>
+						<p>Cats are the most beautiful animals in the world.</p>
 						<button>Sign up</button>
 					</div>
 					<div className='image'>pictrue</div>
-					<div></div>
 				</div>
-				<div className='images'></div>
+				<div className='images'>
+					<div className='random-info'>
+						<h1>Sweet cats</h1>
+					</div>
+					<div className='random-pics'>
+						<div className='pic-text'>
+							<div className='random-pic'></div>
+							<div className='text'>text</div>
+						</div>
+						<div className='pic-text'>
+							<div className='random-pic'></div>
+							<div className='text'>text</div>
+						</div>
+						<div className='pic-text'>
+							<div className='random-pic'></div>
+							<div className='text'>text</div>
+						</div>
+						<div className='pic-text'>
+							<div className='random-pic'></div>
+							<div className='text'>text</div>
+						</div>
+					</div>
+				</div>
 				<div className='quote'></div>
 				<div className='closing'></div>
 			</div>
-      <div className="black-background"></div>
+			<div className='black-background'></div>
 		</div>
 	);
 }
