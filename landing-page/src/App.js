5e5c2b5aed0783fb00de5db32c7a68292016afd6
Pat <patrycja.filipowicz46@gmail.com>
2023-07-28 08:06:13 +0200

    changes.

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<div className='black-background'></div>
 			<div className='container'>
 				<div className='headerLogo'>
-					<div className="baby-cat">BabyCat</div>
+					<div className='baby-cat'>BabyCat</div>
 					<div className='headers'>
 						<div>Link Link Link</div>
 						{/* <div>link 2</div>
@@ -15,14 +15,20 @@ function App() {
 					</div>
 				</div>
 				<div className='introduction'>
-				<div className="space-intro"></div>
+					<div className='space-intro'></div>
 					<div>
-						<h1 className='about'>This app is about cats!</h1>
-						<p>Cats are the most beautiful animals in the world.</p>
-						<button className='button-1'>Sign up</button>
+						<div className='about-image'>
+							<div>
+								<h1 className='about'>This app is about cats!</h1>
+								<p>Cats are the most beautiful animals in the world.</p>
+								<div className='button-1-div'>
+									<button className='button-1'>Sign up</button>
+								</div>
+							</div>
+							<div className='image'>pictrue</div>
+						</div>
+						<div className='space-intro'></div>
 					</div>
-					<div className='image'>pictrue</div>
-					<div className="space-intro"></div>
 				</div>
 				<div className='images'>
 					<div className='random-info'>
@@ -48,26 +54,40 @@ function App() {
 					</div>
 				</div>
 				<div className='quote'>
-				<div className='author'></div>
+					<div className='author'></div>
 					<div className='quote-text'>
-						<p className="quote-1">“In nine lifetimes,<br/></p>
-						<p className="quote-2">you’ll never know as much about your cat<br/></p>
-						<p className="quote-3">as your cat knows about you.”</p>
+						<p className='quote-1'>
+							“In nine lifetimes,
+							<br />
+						</p>
+						<p className='quote-2'>
+							you’ll never know as much about your cat
+							<br />
+						</p>
+						<p className='quote-3'>as your cat knows about you.”</p>
 					</div>
 					<div className='author'>- Michel de Montaigne</div>
 				</div>
+
 				<div className='closing-sing-button'>
 					<div className='blue-space'>
 						<div className='button-text'>
-							<div className="call-to-action">
-								<p className="sign-up-text">Call to action! It's time!<br/></p>
-								<p className="sign-up-text-2">Sign to our product by clicking sign up button</p>
+							<div className='call-to-action'>
+								<p className='sign-up-text'>
+									Call to action! It's time!
+									<br />
+								</p>
+								<p className='sign-up-text-2'>
+									Sign to our product by clicking sign up button
+								</p>
 							</div>
-							<div className="">
-							<button className="button-2">Sign up</button></div>
+							<div className='button-2-div'>
+								<button className='button-2'>Sign up</button>
+							</div>
 						</div>
 					</div>
 				</div>
+				
 				<div className='closing'>
 					<div>Copyright℗ ThepatProject 2023</div>
 				</div>
